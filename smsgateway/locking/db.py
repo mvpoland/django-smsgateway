250df8c49d44e3417c0ad629423582e28a63f108
Kevin Wetzels <kevin@roam.be>
2015-09-14 13:17:57 +0200

    Ensure timeout for db lock >= 0

--- smsgateway/locking/db.py
+++ smsgateway/locking/db.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from locking.exceptions import AlreadyLocked
-from locking.models import NonBlockingLock
+from locking.models import NonBlockingLock, MAX_AGE_FOREVER
 from logging import getLogger
 
 
@@ -15,6 +15,10 @@ class Lock(object):
         self.lock = None
 
     def acquire(self, timeout):
+        if timeout < 0:
+            # django-smsgateway by default uses a negative value to indicate the lock should be acquired until it's
+            # released. Django-locking does not support negative values.
+            timeout = MAX_AGE_FOREVER
         try:
             self.lock = NonBlockingLock.objects.acquire_lock(None, timeout, self.name)
             return True
